b839cf69b17615fe8b03de3bbdb2ef6f4cbd4ee7
zinefer <zinefer@gmail.com>
2019-05-16 14:26:00 -0600

    Generate search controller

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Search Controller
+class SearchController < ApplicationController
+  def index; end
+end
---
+++ test/controllers/search_controller_test.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SearchControllerTest < ActionDispatch::IntegrationTest
+  test 'should get index' do
+    get search_index_url
+    assert_response :success
+  end
+end
