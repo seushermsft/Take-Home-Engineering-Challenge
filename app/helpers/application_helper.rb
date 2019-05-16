8b4d365d0609c274d7fc5a03ce152dd6a9560a73
zinefer <zinefer@gmail.com>
2019-05-16 14:36:41 -0600

    Create basic layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@
 
 # Application Helper
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when :notice  then ''
+    when :success then 'is-success'
+    when :error   then 'is-danger'
+    when :alert   then 'is-warning'
+    end
+  end
 end
